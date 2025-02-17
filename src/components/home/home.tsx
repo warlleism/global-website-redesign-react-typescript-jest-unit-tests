8489cc17a7ab265b398108d5dd8187985892b083
Warllei Martins <warllei.s.martins@hotmail.com>
2025-02-17 13:54:16 -0300

    correções

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -176,8 +176,8 @@ export default function Home() {
                                     <h1 className=" lg:text-[2.6rem] font-bold text-[#0096FB]">{obj.title}</h1>
                                     <p className={`text-[1.7rem] ${obj.title ? "lg:text-[2rem]" : "lg:text-[3rem]"}  text-white font-bold lg:text-start text-center`}>{obj.description}</p>
                                     <StyledLink href={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}>
-                                        <div className="h-full flex text-[.5rem] lg:text-[.9rem] justify-center items-center">Saiba mais</div>
-                                        <div className="h-full flex justify-center items-center"> <IoIosArrowForward color="#fff" size={20} className="animate-pulse" style={{ marginTop: -2 }} /></div>
+                                        <div className="h-full flex text-[.5rem] lg:text-[1.2rem] justify-center items-center">Saiba mais</div>
+                                        <div className="h-full flex justify-center items-center"> <IoIosArrowForward color="#fff" size={20} className="animate-pulse" style={{ marginTop: 2.2}} /></div>
                                     </StyledLink>
                                 </div>
                             </div>
