7278647f01eb8958f64125527ad271cdcf8ede68
Warllei Martins <warllei.s.martins@hotmail.com>
2025-02-17 07:03:02 -0300

    fix: correções

--- src/components/brands/brands.tsx
+++ src/components/brands/brands.tsx
@@ -9,13 +9,15 @@ import Vix from "../../assets/imgs/brands/vix.png"
 import Will from "../../assets/imgs/brands/will.png"
 import { StyledContainer, StyledLink } from "../../styles/styledComponents/styledComponents"
 import { IoIosArrowForward } from "react-icons/io"
-
+import { useEffect } from "react"
 
 export default function Brands() {
+
+
     return (
         <StyledContainer>
-            <div className="flex flex-col lg:flex-row justify-between items-center w-[100%] gap-5 lg:gap-0">
-                <div className="flex flex-col gap-5 w-[100%] lg:w-[40%]">
+            <div className="flex flex-col lg:flex-row justify-between items-center w-full gap-5 lg:gap-0 overflow-hidden">
+                <div className="flex flex-col gap-5 w-full lg:w-[40%]  hidden sm:block ">
                     <div className="flex flex-row justify-between">
                         <img className="w-[80px] lg:w-[120px] transition-all duration-300 hover:scale-110" src={Cooca} alt="" />
                         <img className="w-[80px] lg:w-[120px] transition-all duration-300 hover:scale-110" src={Fort} alt="" />
@@ -32,23 +34,36 @@ export default function Brands() {
                         <img className="w-[80px] lg:w-[120px] transition-all duration-300 hover:scale-110" src={Will} alt="" />
                     </div>
                 </div>
-                <div className="w-[100%] lg:w-[49%] flex flex-col justify-center items-center lg:items-start lg:justify-between gap-5">
+                <div className="w-full lg:w-[49%] flex flex-col justify-center items-center lg:items-start lg:justify-between gap-5">
                     <div className="text-[1.5rem] font-bold text-[#0000007d]">MARCAS PERCEIRAS</div>
                     <div className="mt-5">
-                        <div className="text-[1.3rem] lg:text-[2rem] text-center lg:text-start font-bold">Mova-se mais rápido com</div>
-                        <div className="text-[1.3rem] lg:text-[2rem] text-center lg:text-start font-bold">Com Parceria <span className="text-[#0096FB]">Globalsys</span></div>
-                        <div className="text-[1.4rem] text-center lg:text-start font-bold">Entre em contato com a gente!</div>
+                        <div style={{ marginBottom: -10 }} className="text-[1.3rem] lg:text-[3rem] text-center lg:text-start font-bold text-[#212121]">MOVA-SE MAIS RÁPIDO COM</div>
+                        <div className="text-[1.3rem] lg:text-[3rem] text-center lg:text-start font-bold text-[#212121]">COM PARCEIRA <span className="text-[#0096FB] font-black">GLOBALSYS</span></div>
+                        <div className="text-[1.8rem] text-center lg:text-start font-bold">Entre em contato com a gente!</div>
                     </div>
-                    <div className="mt-5 text-lg">
+                    <div className="mt-5 text-lg w-full w-[80%]">
                         <div className="text-[#4d4d4d] text-center lg:text-start">Nosso especialista está pronto para esclarecer suas dúvidas e ajudá-lo a encontrar as <span className="font-bold text-[#0096FB]">melhores soluções</span> para o seu sucesso.</div>
                         <div className="text-[#4d4d4d] text-center lg:text-start">Com experiência e dedicação, oferecemos <span className="font-bold text-[#0096FB]">suporte personalizado</span> para guiá-lo no caminho certo.</div>
                     </div>
-                    <StyledLink href="https://globalsys.com.br/contato" target="_blank" style={{ width: 300, height: 50 }}>
+                    <StyledLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" style={{ width: 300, height: 50 }}>
                         <div>Entre em contato</div>
                         <div> <IoIosArrowForward color="#fff" size={20} className="animate-pulse" style={{ marginTop: 2 }} /></div>
                     </StyledLink>
                 </div>
+
+                <div className="flex flex-row gap-5 w-full lg:w-[40%] block sm:hidden overflow-x-auto scroll-smooth" >
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Cooca} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Fort} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Grupo} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Kara} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Petz} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Wine} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Reserva} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Vix} alt="" />
+                    <img className="w-[150px] transition-all duration-300 hover:scale-110" src={Will} alt="" />
+                </div>
+
             </div>
-        </StyledContainer>
+        </StyledContainer >
     )
 }
