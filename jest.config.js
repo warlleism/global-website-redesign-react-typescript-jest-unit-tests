58324845e64b295755a2978a7dcfe3fa930ecc31
Warllei Martins <warllei.s.martins@hotmail.com>
2025-02-20 15:29:54 -0300

    🔧 Removidas configurações de teste para Tests com Jest
    remoçoes

--- jest.config.js
+++ jest.config.js
@@ -7,12 +7,6 @@ export default {
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
-  moduleNameMapper: {
-    "\\.(css)$": "identity-obj-proxy",
-    "\\.(jpg|jpeg|png)$": "identity-obj-proxy",
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': 'jest-transform-stub',
-    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-  },
   setupFiles: ["<rootDir>/jest.polyfills.js"],
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
